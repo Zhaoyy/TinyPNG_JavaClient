c00a272e7a1356f5fbe6879735e470ba51f0fd32
zhaoyangyang <867986737@qq.com>
2015-10-10 16:26:25 +0800

    MySource

---
+++ src/main/java/tinify/MySource.java
@@ -0,0 +1,128 @@
+package tinify;
+
+import com.squareup.okhttp.Response;
+import com.tinify.Client;
+import com.tinify.Options;
+import com.tinify.Tinify;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import utils.StringUtil;
+
+/**
+ * MySource
+ *
+ * @author Mislead
+ *         DATE: 2015/10/10
+ *         DESC:
+ **/
+public class MySource {
+
+  private static final String TAG = "MySource";
+
+  /**
+   * image compress method:
+   *
+   * scale: Scales the image down proportionally. You must provide either a target width or a
+   * target height, but not both. The scaled image will have exactly the provided width or height.
+   *
+   * fit: Scales the image down proportionally so that it fits within the given dimensions. You
+   * must
+   * provide both a width and a height. The scaled image will not exceed either of these
+   * dimensions.
+   *
+   * cover: Scales the image proportionally and crops it if necessary so that the result has
+   * exactly
+   * the given dimensions. You must provide both a width and a height. Which parts of the image are
+   * cropped away is determined automatically. An intelligent algorithm determines the most
+   * important areas and leaves these intact
+   */
+
+  public static final String RESIZE_METHOD_SCALE = "scale";
+  public static final String RESIZE_METHOD_FIT = "fit";
+  public static final String RESIZE_METHOD_COVER = "cover";
+
+  private boolean selected = true;
+  private String sourcePath;
+  private String outPath;
+
+  private String url;
+  private Options options = null;
+
+  public MySource(boolean selected, String sourcePath, String outPath) {
+    this.selected = selected;
+    this.sourcePath = sourcePath;
+    this.outPath = outPath;
+  }
+
+  public boolean isSelected() {
+    return selected;
+  }
+
+  public void setSelected(boolean selected) {
+    this.selected = selected;
+  }
+
+  public String getSourcePath() {
+    return sourcePath;
+  }
+
+  public void setSourcePath(String sourcePath) {
+    this.sourcePath = sourcePath;
+  }
+
+  public String getOutPath() {
+    return outPath;
+  }
+
+  public void setOutPath(String outPath) {
+    this.outPath = outPath;
+  }
+
+  public boolean isAllowed() {
+    return sourcePath.endsWith(".png")
+        || sourcePath.endsWith(".jpg")
+        || (getFileSize(sourcePath) % 1024 % 1024) < 3;
+  }
+
+  public long getFileSize(String path) {
+    File file = new File(path);
+    if (!file.exists() || file.isDirectory()) {
+      return 0;
+    }
+
+    return file.length();
+  }
+
+  public String update2GetDownloadUrl() throws IOException {
+    Response response = Tinify.client()
+        .request(Client.Method.POST, "/shrink", Files.readAllBytes(Paths.get(sourcePath)));
+
+    url = response.header("Location");
+    return url;
+  }
+
+  public void setOutImageSize(String method, int width, int height) {
+    if (options == null) {
+      options = new Options();
+    }
+    Options command =
+        new Options().with("method", method).with("width", width).with("height", height);
+    options.with("resize", command);
+  }
+
+  public void getOutFileByUrl() throws IOException {
+    if (StringUtil.isNullOrEmpty(url)) {
+      throw new NullPointerException("url is empty or null!");
+    }
+
+    Response response;
+    if (options == null) {
+      response = Tinify.client().request(Client.Method.GET, url);
+    } else {
+      response = Tinify.client().request(Client.Method.POST, url, options);
+    }
+    Files.write(Paths.get(outPath), response.body().bytes());
+  }
+}
