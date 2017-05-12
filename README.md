4aea3756b9a67532cba058c072fa6353128583d3
mislead <867986737@qq.com>
2017-05-12 15:08:13 +0800

    bug fix

--- README.md
+++ README.md
@@ -9,3 +9,14 @@
 功能：
 - 能够选择一个文件夹下的所有图片，也可以拖拽一组图片进行添加。
 - 自动完成图片的上传和下载压缩后图片。
+
+配置TinyPNG Key& 输出目录
+
+> 你会在jar文件所在目录下有tiny.properties这个配置文件，修改相应的参数即可。
+
+default ``tiny.properties``
+```txt
+#Fri May 12 14:27:37 CST 2017
+outDir=jar文件所在目录\out\
+key= tiny key
+```
--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,10 +1,12 @@
 import com.tinify.Tinify;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Enumeration;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.FontUIResource;
+
 import swing.MainForm;
 import utils.PropertiesUtil;
 import utils.StringUtil;
@@ -19,63 +21,62 @@ import utils.SwingUtil;
  **/
 public class AppMain {
 
-  private static final String defaultKey = "vPhKEx0a6_UZN1Aylky_Lz59m3uUDH38";
-  private static final String defaultOutDir = "out";
-  private static final String KEY = "key";
-  private static final String OUT_DIR = "outDir";
+    private static final String defaultKey = "vPhKEx0a6_UZN1Aylky_Lz59m3uUDH38";
+    private static final String defaultOutDir = "out";
+    private static final String KEY = "key";
+    private static final String OUT_DIR = "outDir";
 
-  public static void main(String args[]) {
+    public static void main(String args[]) {
 
-    initProperties();
+        initProperties();
 
-    try {
-      Tinify.setKey(PropertiesUtil.getPropertiesValue(KEY, defaultKey));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    setWindowGlobalSetting();
-    new MainForm().invokeJFram();
-  }
-
-  private static void setWindowGlobalSetting() {
-    try {
-
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      FontUIResource fontUIResource = new FontUIResource(SwingUtil.font);
-      for (Enumeration keys = UIManager.getDefaults().keys(); keys.hasMoreElements(); ) {
-        Object key = keys.nextElement();
-        Object value = UIManager.get(key);
-        if (value instanceof FontUIResource) {
-          UIManager.put(key, fontUIResource);
-          break;
+        try {
+            Tinify.setKey(PropertiesUtil.getPropertiesValue(KEY, defaultKey));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-      }
-    } catch (ClassNotFoundException ex) {
-      ex.printStackTrace();
-    } catch (InstantiationException ex) {
-      ex.printStackTrace();
-    } catch (IllegalAccessException ex) {
-      ex.printStackTrace();
-    } catch (UnsupportedLookAndFeelException ex) {
-      ex.printStackTrace();
+        setWindowGlobalSetting();
+        new MainForm().invokeJFram();
     }
-  }
 
-  private static void initProperties() {
-    try {
-      String key = PropertiesUtil.getPropertiesValue(KEY, "");
-      if (StringUtil.isNullOrEmpty(key)) {
-        PropertiesUtil.writePropertiesVal(KEY, defaultKey);
-      }
+    private static void setWindowGlobalSetting() {
+        try {
 
-      String outDir = PropertiesUtil.getPropertiesValue(OUT_DIR, "");
-      if (StringUtil.isNullOrEmpty(outDir)) {
-        File file = new File(defaultOutDir);
-        file.mkdirs();
-        PropertiesUtil.writePropertiesVal(OUT_DIR, file.getAbsolutePath());
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            FontUIResource fontUIResource = new FontUIResource(SwingUtil.font);
+            for (Enumeration keys = UIManager.getDefaults().keys(); keys.hasMoreElements(); ) {
+                Object key = keys.nextElement();
+                Object value = UIManager.get(key);
+                if (value instanceof FontUIResource) {
+                    UIManager.put(key, fontUIResource);
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
     }
-  }
+
+    private static void initProperties() {
+        try {
+            String key = PropertiesUtil.getPropertiesValue(KEY, "");
+            if (StringUtil.isNullOrEmpty(key)) {
+                PropertiesUtil.writePropertiesVal(KEY, defaultKey);
+            }
+            File file = new File(defaultOutDir);
+            if (!file.exists()) file.mkdirs();
+            String outDir = PropertiesUtil.getPropertiesValue(OUT_DIR, "");
+            if (StringUtil.isNullOrEmpty(outDir)) {
+                PropertiesUtil.writePropertiesVal(OUT_DIR, file.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
